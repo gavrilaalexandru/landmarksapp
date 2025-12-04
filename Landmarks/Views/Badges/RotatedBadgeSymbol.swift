b8b3e7f8e5c8dc252ccdf2a42938be1bdc81a604
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-12-04 14:56:44 +0200

    badges + categories + hikes

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Alexandru-Valentin Gavrilă on 04.12.2025.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
